16d807085614a7cad228ca0b139c5a4d0fecd9d1
evenifyouforget <idiotvtuber@gmail.com>
2026-04-07 10:28:08 -0400

    more tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -14,9 +14,16 @@ def test_converger_explicit_saturation(in_vec2):
     out_flow = converger_explicit(in_flow)
     assert isclose(np.sum(out_flow), min(np.sum(in_flow), 1))
 
-def test_converger_explicit_not_saturated(in_vec2):
+def test_converger_explicit_output_not_saturated(in_vec2):
     in_flow = np.array(in_vec2)
     if np.sum(in_flow) > 1:
         pytest.skip("Test does not cover this range")
     out_flow = converger_explicit(in_flow)
     assert np.all(isclose(out_flow, in_flow))
+
+def test_converger_explicit_input_all_saturated(in_vec2):
+    in_flow = np.array(in_vec2)
+    if np.any(in_flow < 0.5):
+        pytest.skip("Test does not cover this range")
+    out_flow = converger_explicit(in_flow)
+    assert np.all(isclose(out_flow, [0.5, 0.5]))
