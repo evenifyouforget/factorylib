8a2cc49226082e498b567547e15aa7b18b7a0e62
evenifyouforget <idiotvtuber@gmail.com>
2026-04-07 11:54:58 -0400

    rename symbol

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
-test_data = []
+in2_tuples = []
 
 for i in range(11):
     a = i / 10
     for j in range(11):
         b = j / 10
-        test_data.append((a, b))
+        in2_tuples.append((a, b))
 
 
 def pytest_generate_tests(metafunc):
     if "in_vec2" in metafunc.fixturenames:
-        metafunc.parametrize("in_vec2", test_data)
+        metafunc.parametrize("in_vec2", in2_tuples)
