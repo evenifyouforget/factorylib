b060fb226c47b82b6560e683f402352f201e41c2
evenifyouforget <idiotvtuber@gmail.com>
2026-04-07 13:21:52 -0400

    remaining in3 cases

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -41,6 +41,7 @@ def test_converger_explicit_in2_general(in_vec2):
     in_flow = np.array(in_vec2)
     a, b = in_flow
     out_flow = converger_explicit(in_flow)
+    # Handwritten explicit formula that is already validated
     expect_out_flow = np.array([min(a, 1 - min(b, 0.5)), min(b, 1 - min(a, 0.5))])
     assert np.all(isclose(out_flow, expect_out_flow))
 
@@ -74,4 +75,24 @@ def test_converger_explicit_in3_2_of_3(in_vec2):
     for zero_pos in range(3):
         in_flow = np.insert(in_flow_2, zero_pos, 0)
         expected = np.insert(out_flow_2, zero_pos, 0)
+        assert np.all(isclose(converger_explicit(in_flow), expected))
+
+
+def test_converger_explicit_in3_cherrypick(in_vec3):
+    a, b, c = in_vec3
+    if not 0 < a < min([1 / 3, b, c]):
+        pytest.skip("Test does not cover this range")
+    # a always gets its full share, so b and c must fight for the remaining portion (1 - a)
+    # this reduces to a 2-input subproblem
+    # since the solver is normalized to 1 item/s, we need to scale
+    # b, c -> 1 - a
+    # up to
+    # b / (1 - a), c / (1 - a) -> 1
+    subproblem_scale = 1 - a
+    subproblem_in_flow_2 = np.array([b, c]) / subproblem_scale
+    in_flow_2 = np.array([b, c])
+    out_flow_2 = converger_explicit(subproblem_in_flow_2) * subproblem_scale
+    for a_pos in range(3):
+        in_flow = np.insert(in_flow_2, a_pos, a)
+        expected = np.insert(out_flow_2, a_pos, a)
         assert np.all(isclose(converger_explicit(in_flow), expected))
